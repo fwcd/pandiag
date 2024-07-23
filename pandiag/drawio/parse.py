65a3225a2e29958ec91c02e51a94977422e93929
fwcd <fwcd@fwcd.dev>
2024-07-23 19:51:43 +0200

    Parse compressed drawio XML

--- pandiag/drawio/parse.py
+++ pandiag/drawio/parse.py
@@ -1,9 +1,18 @@
 from pandiag.model.graph import Graph
 
+import base64
+import urllib.parse
 import xml.etree.ElementTree as ET
+import zlib
 
 def parse(s: str) -> Graph:
-    element = ET.fromstring(s)
-    print(element.find('diagram[name="New"]'))
+    root_element = ET.fromstring(s)
+
+    diag_compressed = base64.b64decode(root_element.find('diagram[@name="New"]').text)
+    diag_decompressed = zlib.decompress(diag_compressed, wbits=-15)
+    diag_xml = urllib.parse.unquote(diag_decompressed)
+    diag_element = ET.fromstring(diag_xml)
+
+    print(diag_xml)
 
     raise NotImplementedError()

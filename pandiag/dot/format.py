e6125160df40258dda95d5eb4eaa03c867a52166
fwcd <fwcd@fwcd.dev>
2024-07-23 21:14:08 +0200

    Implement DOT formatting for subgraphs

--- pandiag/dot/format.py
+++ pandiag/dot/format.py
@@ -1,16 +1,26 @@
+from typing import Optional
 from pandiag.model.graph import Edge, Graph, Subgraph
 from pandiag.utils import indent
 
+def _format_node(label: Optional[str]) -> str:
+    return f'"{label}"' if label else None
+
 def _format_edge(edge: Edge, graph: Graph) -> str:
-    return f"{edge.source} {'->' if graph.directed else '--'} {edge.dest};"
+    return f"{_format_node(edge.source)} {'->' if graph.directed else '--'} {_format_node(edge.dest)};"
 
 def _format_subgraph(subgraph: Subgraph, graph: Graph) -> list[str]:
-    # TODO: Subgraphs
-    return [_format_edge(e, graph=graph) for e in subgraph.edges]
+    return [
+        *(_format_edge(e, graph=graph) for e in subgraph.edges),
+        *(l for g in subgraph.subgraphs for l in [
+            f'subgraph {_format_node(g.name)} {{',
+            *indent(_format_subgraph(g, graph)),
+            '}',
+        ]),
+    ]
 
 def _format_graph(graph: Graph) -> list[str]:
     return [
-        f"{'digraph' if graph.directed else 'graph'} {{",
+        f"{'digraph' if graph.directed else 'graph'} {_format_node(graph.rootgraph.name)} {{",
         *indent(_format_subgraph(graph.rootgraph, graph=graph)),
         '}',
     ]

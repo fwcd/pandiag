4b4b9e542960b18699f2a3588c592d1160bc6a1f
fwcd <fwcd@fwcd.dev>
2024-07-23 21:13:54 +0200

    Parse subgraphs from drawio

--- pandiag/drawio/parse.py
+++ pandiag/drawio/parse.py
@@ -1,4 +1,6 @@
-from pandiag.model.graph import Edge, Graph
+from __future__ import annotations
+from dataclasses import dataclass, field
+from pandiag.model.graph import Edge, Graph, Subgraph
 
 import base64
 import re
@@ -6,28 +8,64 @@ import urllib.parse
 import xml.etree.ElementTree as ET
 import zlib
 
+@dataclass
+class _Cell:
+    element: ET.Element
+    childs: list[_Cell] = field(default_factory=list)
+
 def _strip_html(raw: str) -> str:
     # TODO: Be cleverer about this, perhaps use a proper HTML parser
     raw = raw.replace('<br>', ' ')
     raw = re.sub(f'<[^>]+>', '', raw)
     return raw
 
-def _parse_graph(element: ET.Element) -> Graph:
-    graph = Graph(directed=True)
-    cells = {cell.attrib['id']: cell for cell in element.findall('.//mxCell')}
+def _construct_subgraph(cells: list[_Cell], cells_by_id: dict[str, _Cell]) -> Subgraph:
+    subgraph = Subgraph()
 
     # TODO: Perhaps support nodes and ids so we don't have to write the full labels into each edge?
-    for cell in cells.values():
-        if cell.attrib.get('edge') == '1':
-            source = cells[cell.attrib['source']] if 'source' in cell.attrib else None
-            target = cells[cell.attrib['target']] if 'target' in cell.attrib else None
+    for cell in cells:
+        if cell.element.attrib.get('edge') == '1':
+            source = cells_by_id[cell.element.attrib['source']] if 'source' in cell.element.attrib else None
+            target = cells_by_id[cell.element.attrib['target']] if 'target' in cell.element.attrib else None
 
-            graph.rootgraph.edges.append(Edge(
-                source=_strip_html(source.get('value')) if source else None,
-                dest=_strip_html(target.get('value')) if target else None,
+            subgraph.edges.append(Edge(
+                source=_strip_html(source.element.get('value')) if source else None,
+                dest=_strip_html(target.element.get('value')) if target else None,
             ))
+        
+        if len(cell.childs) > 0:
+            subsubgraph = _construct_subgraph(
+                cells=cell.childs,
+                cells_by_id=cells_by_id,
+            )
+            subsubgraph.name = cell.element.get('value')
+            if subsubgraph.name:
+                subgraph.subgraphs.append(subsubgraph)
+            else:
+                # Merge the subsubgraph into the current one
+                subgraph.edges += subsubgraph.edges
+                subgraph.subgraphs += subsubgraph.subgraphs
+    
+    return subgraph
 
-    return graph
+def _parse_graph(element: ET.Element) -> Graph:
+    cells_by_id = {cell.attrib['id']: _Cell(cell) for cell in element.findall('.//mxCell')}
+    root_cells: list[_Cell] = []
+
+    # Construct tree
+    for id, cell in list(cells_by_id.items()):
+        if parent := cell.element.attrib.get('parent'):
+            cells_by_id[parent].childs.append(cell)
+        else:
+            root_cells.append(cell)
+
+    return Graph(
+        directed=True,
+        rootgraph=_construct_subgraph(
+            cells=root_cells,
+            cells_by_id=cells_by_id
+        ),
+    )
 
 def parse(raw: str) -> Graph:
     root_element = ET.fromstring(raw)
